562451568483e92cabd553043311a9934082f01d
= <=>
2022-12-10 17:28:24 +0300

    adding font awesome

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ function App() {
       <p className='class1'>Login</p>
       <br/>
       <input className='input1' type="email" placeholder='Email*' required/>
+      <br></br>
+      <br></br>
+      <input class="input1" type="password" placeholder=" Password*" required />
+      <h5>Forget your password?</h5>
+      <input class="button1" type="submit" value="Log in"/>
+      <span><input class="checkbox" type="checkbox"/> Remember me</span>
+      <br></br>
+      <br></br>
+      <fieldset>
+        <legend>or</legend>
+    </fieldset>
     </div>
     </form>
 
