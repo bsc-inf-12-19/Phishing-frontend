935fe6cf7086064b8708f523a5979e355a3e41c7
= <=>
2022-12-10 17:06:06 +0300

    starting OWASP Juice shop project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+//import './App.css';
+import {BrowserRouter as Router,Route ,Routes} from 'react-router-dom'
+
+function App() {
+  return (
+    <h1>Hello</h1>
+
+  );
+}
+
+export default App;
