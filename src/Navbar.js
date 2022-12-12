9569e33eed71573a3bac7ef98209417e962220c3
= <=>
2022-12-12 18:02:09 +0300

    modfying app.js file

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import "./Navbar.css"
+function Navbar (){
+    return(
+        <nav>
+            <ul>
+                <li><img src ="juiceShop_Logo.png"/></li>
+                <li><p>OWASP Juice Shop</p></li>
+            </ul>
+            {/* <div  className="list1">
+            <ul>
+                <li><p>Account En</p></li>
+                <li><p>En</p></li>
+            </ul>
+            </div> */}
+        </nav>
+    )
+}
+export default Navbar
