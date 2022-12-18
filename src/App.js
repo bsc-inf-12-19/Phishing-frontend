a5c83734ccd1c6b3036511902d8e5b62e84d54aa
= <=>
2022-12-18 23:39:45 +0300

    editing app.js and navbar.js

--- src/App.js
+++ src/App.js
@@ -1,71 +1,44 @@
-
-import './App.css';
 import React from 'react';
-import { Link, useNavigate } from "react-router-dom";
+
+// import { IconName } from "react-icons/fa";
+import './App.css';
+// import { IconName } from "react-icons/fa";
+// import { Link, useNavigate } from "react-router-dom";
 import Navbar from './Navbar';
 // import {useState} from 'react';
 // import './all.css.';
 // import './fontawesome-free-6.1.2-web\css\all.css';
 // import {BrowserRouter as Router,Route ,Routes} from 'react-router-dom'
-
 function App() {
-  const [formState, setFormState] = React.useState({
-    email: "",
-    password: ""
-  })
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>, type: string) => {
-    e.preventDefault();
-    setFormState({
-      ...formState,
-      [type]: e.target.value
-    })
-  }
-const getErrors = (): string[] => {
-    const errors = [];
-    if (!formState.username) errors.push("Name required");
-    if (!formState.email) {
-      errors.push("Email required");
-    } else if (!/^\S+@\S+\.\S+$/.test(formState.email)) {
-      errors.push("Invalid email");
-    }
-    if (!formState.password) errors.push("Password required");
-    return errors;
-  }
-const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const errors = getErrors();
-    for (let error of errors) {
-      alert(error);
-    }
-    if (errors.length) return;
-    const response = await fetch('http://localhost:8000/api/v1/signin', {
-      method: "post",
-      headers: {
-      },
-      body: JSON.stringify({
-        email: formState.email,
-        password: formState.password
-      })
-    })
-    if (response.status !== 200) {
-      return alert("Something went wrong.");
-    }
-  }
+
+  // const [formState, setFormState] = React.useState({
+ 
+  //   const response = await fetch('http://localhost:8000/api/v1/signin', {
+  //     method: "post",
+  //     headers: {
+  //     },
+  //     body: JSON.stringify({
+  //       email: formState.email,
+  //       password: formState.password
+  //     })
+  //   })
+  
   return (
     
     // <form action={""} method="POST" target="_black">
       <form>
       <Navbar/>
       <br></br>
+      <br></br>
     <div className='div1'>
       <p className='class1'>Login</p>
       <br/>
       <input className='input1' type="email" placeholder="  Email *" 
-       onChange={(e) => onChange(e, "email")} value={formState.email} required/>
+        required/>
       <br></br>
       <br></br>
       <input class="input1" type="password" placeholder="  Password *" 
-      onChange={(e) => onChange(e, "password")} value={formState.password} required />
+       required />
       <h5>Forget your password?</h5>
       <input class="button1" type="submit" value="Log in"/>
       <span><input class="checkbox" type="checkbox"/> Remember me</span>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,17 @@
 import "./Navbar.css"
+// import { IconName } from "react-icons/fa";
 function Navbar (){
     return(
         <nav>
+
+            {/* my hamberger very userfull */}
             <ul>
-                <li><img src ="juiceShop_Logo.png"/></li>
+            <li><svg viewBox="0 0 100 80" width="30" height="30">
+              <rect width="70" height="10"></rect>
+              <rect y="25" width="70" height="10"></rect>
+              <rect y="50" width="70" height="10"></rect>
+           </svg></li>
+                <li><img alt="logo" src ="juiceShop_Logo.png"/></li>
                 <li><p>OWASP Juice Shop</p></li>
             </ul>
             {/* <div  className="list1">
