92b8130ddabae0aec4d08f450ef2ca2e5bd086ee
= <=>
2022-12-19 03:11:49 +0300

    modifying files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-
-// import { IconName } from "react-icons/fa";
 import './App.css';
-// import { IconName } from "react-icons/fa";
 // import { Link, useNavigate } from "react-router-dom";
 import Navbar from './Navbar';
 // import {useState} from 'react';
-// import './all.css.';
-// import './fontawesome-free-6.1.2-web\css\all.css';
-// import {BrowserRouter as Router,Route ,Routes} from 'react-router-dom'
 function App() {
 
   // const [formState, setFormState] = React.useState({
@@ -24,35 +18,31 @@ function App() {
   //   })
   
   return (
-    
-    // <form action={""} method="POST" target="_black">
       <form>
-      <Navbar/>
-      <br></br>
-      <br></br>
-    <div className='div1'>
-      <p className='class1'>Login</p>
-      <br/>
-      <input className='input1' type="email" placeholder="  Email *" 
-        required/>
-      <br></br>
-      <br></br>
-      <input class="input1" type="password" placeholder="  Password *" 
-       required />
-      <h5>Forget your password?</h5>
+          <Navbar/>
+           <br></br>
+           <br></br>
+        <div className='div1'>
+           <p className='class1'>Login</p>
+           <br/>
+        <input className='input1' type="email" placeholder="  Email *" required/>
+         <br></br>
+         <br></br>
+       <input class="input1" type="password" placeholder="  Password *" required />
+        <h5>Forget your password?</h5>
       <input class="button1" type="submit" value="Log in"/>
-      <span><input class="checkbox" type="checkbox"/> Remember me</span>
-      <br></br>
-      <br></br>
+        <span><input class="checkbox" type="checkbox"/> Remember me</span>
+         <br></br>
+         <br></br>
       <fieldset>
         <legend>or</legend>
-    </fieldset>
-    <button href="www.google.com" className="Google"><img class="logo" alt='google logo' src="google.svg"/> Log in with Google</button>
-    <br></br>
-    <p class="class2">Not yet a customer?</p>
-    <br></br>
-    </div>
-    </form>
+      </fieldset>
+       <button href="www.google.com" className="Google"><img class="logo" alt='google logo' src="google.svg"/> Log in with Google</button>
+        <br></br>
+      <p class="class2">Not yet a customer?</p>
+        <br></br>
+       </div>
+     </form>
 
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,8 @@
 import "./Navbar.css"
-// import { IconName } from "react-icons/fa";
 function Navbar (){
     return(
         <nav>
-
-            {/* my hamberger very userfull */}
             <ul>
-            {/* <li><svg viewBox="0 0 100 80" width="30" height="30">
-              <rect width="70" height="10"></rect>
-              <rect y="25" width="70" height="10"></rect>
-              <rect y="50" width="70" height="10"></rect>
-           </svg></li> */}
            <li><img alt="menu" class="menu" src="menu.svg"/></li>
                 <li><img alt="logo" src ="juiceShop_Logo.png"/></li>
                 <li class="text"><p>OWASP Juice Shop</p></li>
@@ -20,12 +12,6 @@ function Navbar (){
                 <li><img class="image1" alt="logo" src ="user.svg"/>Account</li>
                 <li><img class="image1" alt="logo" src ="language.svg" />EN</li>
             </ul>
-            {/* <div  className="list1">
-            <ul>
-                <li><p>Account En</p></li>
-                <li><p>En</p></li>
-            </ul>
-            </div> */}
         </nav>
     )
 }
