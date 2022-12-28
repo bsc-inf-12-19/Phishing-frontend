618a79f4549cbfeccb1853d2fde0b76f6944adde
= <=>
2022-12-28 10:23:09 +0300

    modifying index.html, app.css, app.js

--- src/App.js
+++ src/App.js
@@ -39,33 +39,37 @@ function App() {
   return (
     <div class="container">
       <form>
-          <Navbar/>
+        <Navbar/>
            <br></br>
            <br></br>
         <div className='div1'>
            <p className='class1'>Login</p>
            <br/>
         <input class='input1' style={{height:"35%"}} type="email" placeholder="  Email *" required value={Email}
-                    onChange = {(e) => setEmail(e.target.value)}/>
-         <br></br>
-         <br></br>
+                  onChange = {(e) => setEmail(e.target.value)}/>
+                  
+           <br></br>
+           <br></br>
        <input class="input1" style={{height:"35%"}} type="password" placeholder="  Password *" required value={password}
-                    onChange ={(e) => setPassword(e.target.value)}/>
-        <h5>Forget your password?</h5>
+                  onChange ={(e) => setPassword(e.target.value)}/>
+
+         <h5>Forget your password?</h5>
         <button class="button2" style={{height:"25%"}}><img class="logo" alt='google logo' src="login.svg"/><span> Log in</span></button>
-        <span class="span1"><input class="checkbox" type="checkbox"/> Remember me</span>
+         <span class="span1"><input class="checkbox" type="checkbox"/> Remember me</span>
+           <br></br>
+        <fieldset>
+          <legend> or </legend>
+        </fieldset>
+          <br></br>
+        <button href="www.google.com" class ="Google" style={{height:"25%"}}><img class="logo" alt='google logo'src="google.svg"/> Log in with Google</button>
+          <br></br>
+          <div>
+        <p class="class2">Not yet a customer?</p>
+          </div>
          <br></br>
-      <fieldset>
-        <legend> or </legend>
-      </fieldset>
-      <br></br>
-       <button href="www.google.com" class ="Google" style={{height:"25%"}}><img class="logo" alt='google logo' src="google.svg"/> Log in with Google</button>
-        <br></br>
-      <p class="class2">Not yet a customer?</p>
-        <br></br>
-       </div>
-     </form>
-     </div>
+        </div>  
+      </form>
+    </div>
 
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ function Navbar (){
                 <li><img alt="logo" src ="juiceShop_Logo.png"/></li>
                 <li class="text"><p>OWASP Juice Shop</p></li>
             </ul>
-            <ul class="ul2">
+            <ul class="ultwo">
                 <li><input class="inputlogo" type="text"/></li>
                 <li><img class="image1" alt="logo" src ="user.svg"/>Account</li>
                 <li><img class="image1" alt="logo" src ="language.svg" />EN</li>
